db3ffb33712aaf1c5e1333d78087d96e428bce8c
jtdoud <jtdoud@gmail.com>
2014-10-22 20:21:18 -0700

    Added reactive expression

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -3,42 +3,44 @@ library(ggplot2)
 library(scales)
 
 my_theme <- theme_bw() +
-        theme(panel.border = element_blank(),
-              axis.ticks = element_blank(),
-              panel.grid.minor = element_blank(),
-              panel.grid.major.x = element_blank(),
-              panel.grid.major.y = element_line(size = 1),
-              axis.title = element_text(colour = "grey40"),
-              plot.title = element_text(vjust = 2.0))
+    theme(panel.border = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(size = 1),
+          axis.title = element_text(colour = "grey40"),
+          plot.title = element_text(vjust = 2.0))
 
 load("./Data/appData.Rdata")
 
 shinyServer(
-
-        function(input, output) {
-         
-                output$plot <- renderPlot({
-                        
-                        if (input$extreme == T)
-                                temp <- filter(data, BestFatalityEstimate < 500000)
-                        else temp <- data
-                        
-                        temp <- switch(input$actor,
-                                       "Both" = group_by(temp, Region, Year) %>%
-                                               summarise(Fatalities = sum(BestFatalityEstimate)),
-                                       "Government" = filter(temp, IsGovernmentActor == 1) %>%
-                                               group_by(Region, Year) %>%
-                                               summarise(Fatalities = sum(BestFatalityEstimate)),
-                                       "Non-government" = filter(temp, IsGovernmentActor == 0) %>%
-                                               group_by(Region, Year) %>%
-                                               summarise(Fatalities = sum(BestFatalityEstimate))
-                                       )
-
-                        ggplot(data = temp, aes(x= Year, y=Fatalities, color = Region)) +
-                                geom_line(size = 1) + 
-                                scale_y_continuous(labels = comma) + 
-                                my_theme
-                })
-                
-        }      
+    
+    function(input, output) {
+        
+        filt1 <- reactive({
+            if (input$extreme == T)
+                return(filter(data, BestFatalityEstimate < 500000))
+            else return(data)
+        })
+        
+        output$plot <- renderPlot({
+            
+            
+            filt2 <- switch(input$actor,
+                            "Both" = group_by(filt1(), Region, Year) %>%
+                                summarise(Fatalities = sum(BestFatalityEstimate)),
+                            "Government" = filter(filt1(), IsGovernmentActor == 1) %>%
+                                group_by(Region, Year) %>%
+                                summarise(Fatalities = sum(BestFatalityEstimate)),
+                            "Non-government" = filter(filt1(), IsGovernmentActor == 0) %>%
+                                group_by(Region, Year) %>%
+                                summarise(Fatalities = sum(BestFatalityEstimate))
+            )
+            
+            ggplot(data = filt2, aes(x= Year, y=Fatalities, color = Region)) +
+                geom_line(size = 1) + scale_y_continuous(labels = comma) + 
+                my_theme
+        })
+        
+    }      
 )
