1202c091019e62de60f68d940d68018df344528a
jtdoud <jtdoud@gmail.com>
2014-10-22 19:42:49 -0700

    Changed to time series

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -1,5 +1,15 @@
 library(dplyr)
 library(ggplot2)
+library(scales)
+
+my_theme <- theme_bw() +
+        theme(panel.border = element_blank(),
+              axis.ticks = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.major.y = element_line(size = 1),
+              axis.title = element_text(colour = "grey40"),
+              plot.title = element_text(vjust = 2.0))
 
 load("./Data/appData.Rdata")
 
@@ -14,18 +24,20 @@ shinyServer(
                         else temp <- data
                         
                         temp <- switch(input$actor,
-                                       "Both" = group_by(temp, Region) %>%
+                                       "Both" = group_by(temp, Region, Year) %>%
                                                summarise(Fatalities = sum(BestFatalityEstimate)),
                                        "Government" = filter(temp, IsGovernmentActor == 1) %>%
-                                               group_by(Region) %>%
+                                               group_by(Region, Year) %>%
                                                summarise(Fatalities = sum(BestFatalityEstimate)),
                                        "Non-government" = filter(temp, IsGovernmentActor == 0) %>%
-                                               group_by(Region) %>%
+                                               group_by(Region, Year) %>%
                                                summarise(Fatalities = sum(BestFatalityEstimate))
                                        )
 
-                        ggplot(data = temp, aes(x= Region, y=Fatalities/1000)) +
-                                geom_bar(stat = "identity")
+                        ggplot(data = temp, aes(x= Year, y=Fatalities, color = Region)) +
+                                geom_line(size = 1) + 
+                                scale_y_continuous(labels = comma) + 
+                                my_theme
                 })
                 
         }      
